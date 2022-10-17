76c16a2c9795801222e3661ac9fc4bff708d08aa
Ekrem Tayanç <ekremtayanc@gmail.com>
2022-10-17 22:27:04 +0300

    Last Check Publish

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\PostStudentsRequest;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
 use Yajra\DataTables\DataTables;
 
@@ -14,15 +14,16 @@ class StudentController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
     public function index()
     {
         //
         $students = DB::select('CALL spStudentsList');
         return Datatables::of($students)->addIndexColumn()
-            ->addColumn('action', function($row){
-                $btn = '<a href="javascript:void(0)" data-id="'.$row->id.'" class="edit btn btn-primary">Düzenle</a> <a href="javascript:void(0)" data-id="'.$row->id.'" class="delete btn btn-danger">Sil</a>';
+            ->addColumn('action', function ($row) {
+                $btn = '<a href="javascript:void(0)" data-id="' . $row->id . '" class="edit btn btn-primary">Düzenle</a>
+                        <a href="javascript:void(0)" data-id="' . $row->id . '" class="delete btn btn-danger">Sil</a>';
                 return $btn;
             })
             ->rawColumns(['action'])
@@ -34,20 +35,21 @@ class StudentController extends Controller
      * Store a newly created resource in storage.
      *
      * @param PostStudentsRequest $request
-     * @return Response
+     * @return array
      */
     public function store(PostStudentsRequest $request)
     {
         //
-        $student = DB::select('CALL spStudentsCreate(?,?,?,?,?)',array($request->identity_number,$request->student_name,$request->student_surname,$request->school_name,$request->student_number));
-        return ['success' => true, 'message'=>'Öğrenci Kaydı Eklendi.'];
+        $student = DB::select('CALL spStudentsCreate(?,?,?,?,?)',
+                [$request->identity_number, $request->student_name, $request->student_surname, $request->school_name, $request->student_number]);
+        return ['success' => true, 'message' => 'Öğrenci Kaydı Eklendi.'];
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return void
      */
     public function show($id)
     {
@@ -59,25 +61,26 @@ class StudentController extends Controller
      *
      * @param PostStudentsRequest $request
      * @param int $id
-     * @return Response
+     * @return array
      */
-    public function update($id,PostStudentsRequest $request)
+    public function update($id, PostStudentsRequest $request)
     {
         //
-        $student = DB::select('CALL spStudentsUpdate(?,?,?,?,?,?)',array($id,$request->identity_number,$request->student_name,$request->student_surname,$request->school_name,$request->student_number));
-        return ['success'=>true,'message'=>'Öğrenci Kaydı Güncellendi.'];
+        $student = DB::select('CALL spStudentsUpdate(?,?,?,?,?,?)',
+            [$id, $request->identity_number, $request->student_name, $request->student_surname, $request->school_name, $request->student_number]);
+        return ['success' => true, 'message' => 'Öğrenci Kaydı Güncellendi.'];
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return array
      */
     public function destroy(Request $request)
     {
         //
-        $student = DB::select('CALL spStudentsDelete('.$request->id.')');
-        return ['success'=>true,'message'=>'Öğrenci Kaydı Silindi.'];
+        $student = DB::select('CALL spStudentsDelete(' . $request->id . ')');
+        return ['success' => true, 'message' => 'Öğrenci Kaydı Silindi.'];
     }
 }
